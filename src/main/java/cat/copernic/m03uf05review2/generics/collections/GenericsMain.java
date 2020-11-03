a5a6463e0c4c88669d39702a910a490868571226
Arnau Cruz <cruzcambra.arnau@gmail.com>
2020-11-03 21:45:54 +0100

    Proyecto Excepciones

--- src/main/java/cat/copernic/m03uf05review2/generics/collections/GenericsMain.java
+++ src/main/java/cat/copernic/m03uf05review2/generics/collections/GenericsMain.java
@@ -21,8 +21,14 @@ public class GenericsMain {
      * Escribe un método que retorne el ultimo elemento de una lista
      */
     public static <T> T ultimo(List<T> lista) {
-        // ...
-        return null;
+        
+        if(!lista.isEmpty()){
+            return lista.get(lista.size()-1);
+        }else{
+            return null;
+        }
+            
+        
     }
     
     
@@ -30,7 +36,13 @@ public class GenericsMain {
      * Escribe una método que retorne la media de una lista de Double
      */
     public static double media(List<Double> lista) {
-        return 0.0;
+        
+        double n = 0;
+            
+        for (int i = 0; i < lista.size(); i++) {
+            n = (n + lista.get(i)); 
+        }
+        return n / lista.size();
     }
     
     
@@ -38,11 +50,39 @@ public class GenericsMain {
      * Escribe un método que retorne la cadena mas larga de una lista de String
      */
     
+    public static String largo(List<String> lista){
+        
+        if(lista.size() != 0){
+            String resultat = lista.get(0);
+            for(int x = 0; x < lista.size(); x++){
+                if(lista.get(x).length() > resultat.length())
+                resultat = lista.get(x);
+            }
+            return resultat;
+            
+        }else{return null;}
+    }
+    
     /**
      * Escribe un método que retorne el maximo de una lista de enteros
      */
     
+    public static Integer maximo(List<Integer> lista){
     
+        if(lista.size() != 0){
+            
+            int maximo = lista.get(0);
+            
+            for(int x = 0; x < lista.size(); x++){
+                if(lista.get(x) > maximo){
+                    maximo = lista.get(x);
+                }
+            }
+            
+            return maximo;
+            
+        }else{return null;}
+    }
 
     /**
      * @param args the command line arguments
@@ -58,8 +98,40 @@ public class GenericsMain {
         lista1.add("Joan");   
         lista1.add("Carles");   
         lista1.add("Pep");   
-
-        if (lista1.isEmpty()) {
+        
+        ultimo(lista1);
+        
+        List<Double> lista2 = new ArrayList<>();
+        lista2.add(5.5);
+        lista2.add(4.4);   
+        lista2.add(3.3);   
+        lista2.add(2.2);   
+        lista2.add(1.1);
+        
+        media(lista2);
+        
+        List<String> lista3 = new ArrayList<>();
+        lista3.add("Se lo ha cortado");
+        lista3.add("Ramón Rodriguez");   
+        lista3.add("Porque");   
+        lista3.add("No tiene rabo");   
+        lista3.add("El perro de san roque");
+        
+        largo(lista3);
+        
+        List<Integer> lista4 = new ArrayList<>();
+        lista4.add(5);
+        lista4.add(4);   
+        lista4.add(3);   
+        lista4.add(2);   
+        lista4.add(1);
+        
+        maximo(lista4);
+        
+        System.out.println("El ultimo es "+ultimo(lista1)+" la media es "+media(lista2)+" la més llarga es "+largo(lista3)+ " El maximo es " + maximo(lista4));
+        
+        
+        /*if (lista1.isEmpty()) {
             System.out.println("Lista es buida");
         } else {
             System.out.println("Lista te " + lista1.size() + " elements.");
@@ -75,7 +147,9 @@ public class GenericsMain {
             System.out.println(it.next());
         }
         
-        List<String> l2 = lista1.subList(0, lista1.size()/ 2);
+        List<String> l2 = lista1.subList(0, lista1.size()/ 2);*/
+        
+        
         
     }
     
