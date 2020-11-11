4a24beb0a206486b0e4b734c7341b57596237be8
Arnau Cruz <cruzcambra.arnau@gmail.com>
2020-11-11 12:26:41 +0100

    Mejorando MatcherImp

--- src/main/java/cat/copernic/m03uf05review2/generics/collections/Ex/ERxerciciMain.java
+++ src/main/java/cat/copernic/m03uf05review2/generics/collections/Ex/ERxerciciMain.java
@@ -22,9 +22,11 @@ public class ERxerciciMain{
        candidatos.add(new Persona(2, Sexo.MUJER, 25, false, OrientacionSexual.LESBIANA, Formacion.CATEDRATICO, OrientacionPolitica.IZQUIERDA));
        candidatos.add(new Persona(3, Sexo.HOMBRE, 40, false, OrientacionSexual.GAY, Formacion.GRADO_MEDIA, OrientacionPolitica.EXTREMA_DERECHA));
        candidatos.add(new Persona(4, Sexo.MUJER, 29, false, OrientacionSexual.BISEXUAL, Formacion.GRADO_SUPERIOR, OrientacionPolitica.EXTREMA_IZQUIERDA));
+       candidatos.add(new Persona(5, Sexo.HOMBRE, 40, true, OrientacionSexual.BISEXUAL, Formacion.GRADO_SUPERIOR, OrientacionPolitica.EXTREMA_IZQUIERDA));
+       candidatos.add(new Persona(6, Sexo.MUJER, 54, true, OrientacionSexual.HETERO, Formacion.GRADO_SUPERIOR, OrientacionPolitica.EXTREMA_DERECHA));
+       candidatos.add(new Persona(7, Sexo.MUJER, 24, true, OrientacionSexual.HETERO, Formacion.GRADO_SUPERIOR, OrientacionPolitica.EXTREMA_DERECHA));
        
-       
-       Persona aux = new Persona(5, Sexo.MUJER, 39, false, OrientacionSexual.BISEXUAL, Formacion.GRADO_SUPERIOR, OrientacionPolitica.EXTREMA_IZQUIERDA);      
+       Persona aux = new Persona(99, Sexo.HOMBRE, 24, true, OrientacionSexual.HETERO, Formacion.GRADO_SUPERIOR, OrientacionPolitica.EXTREMA_DERECHA);      
        MatcherImpl m = new MatcherImpl(aux, candidatos);
        
        System.out.println("La parella compatible es => "+ m.matches(m.getPersona(), m.getCandidatos())+" amb "+aux);
--- src/main/java/cat/copernic/m03uf05review2/generics/collections/Ex/MatcherImpl.java
+++ src/main/java/cat/copernic/m03uf05review2/generics/collections/Ex/MatcherImpl.java
@@ -14,30 +14,149 @@ public class MatcherImpl implements Matcher {
         
         for(int x = 0; x < candidatos.size(); x++){
             
-            if(candidatos.get(x).getOrientacionSexual() == persona.getOrientacionSexual()){
-                aux = candidatos.get(x);
+            if(candidatos.get(x).getEdad() <= persona.getEdad()+5){
+                if(candidatos.get(x).isFumador() == true && persona.isFumador() == true){
+                    if(sexualitat(candidatos.get(x), persona) == true){
+                        if(politica(candidatos.get(x), persona) == true){
+                            aux = candidatos.get(x);
+                        }
+                        else{
+                            aux = candidatos.get(x);
+                        }
+
+                    }
+                }else if(candidatos.get(x).isFumador() == false && persona.isFumador() == false){
+                    if(sexualitat(candidatos.get(x), persona) == true){
+                        if(politica(candidatos.get(x), persona) == true){
+                            aux = candidatos.get(x);
+                        }
+                        else{
+                            aux = candidatos.get(x);
+                        }
+                    }
+                }else{
+                    if(sexualitat(candidatos.get(x), persona) == true){
+                        if(politica(candidatos.get(x), persona) == true){
+                            aux = candidatos.get(x);
+                        }
+                        else{
+                            aux = candidatos.get(x);
+                        }
+                    }
+                }
+            }else{
+                if(candidatos.get(x).isFumador() == true && persona.isFumador() == true){
+                    if(sexualitat(candidatos.get(x), persona) == true){
+                        if(politica(candidatos.get(x), persona) == true){
+                            aux = candidatos.get(x);
+                        }
+                        else{
+                            aux = candidatos.get(x);
+                        }
+
+                    }
+                }else if(candidatos.get(x).isFumador() == false && persona.isFumador() == false){
+                    if(sexualitat(candidatos.get(x), persona) == true){
+                        if(politica(candidatos.get(x), persona) == true){
+                            aux = candidatos.get(x);
+                        }
+                        else{
+                            aux = candidatos.get(x);
+                        }
+                    }
+                }else{
+                    if(sexualitat(candidatos.get(x), persona) == true){
+                        if(politica(candidatos.get(x), persona) == true){
+                            aux = candidatos.get(x);
+                        }
+                        else{
+                            aux = candidatos.get(x);
+                        }
+                    }
+                }
             }
-            else if(persona.getSexo() == Sexo.HOMBRE && persona.getOrientacionSexual() == OrientacionSexual.BISEXUAL
-                    && candidatos.get(x).getOrientacionSexual() == OrientacionSexual.GAY){
-                aux = candidatos.get(x);
-            }
-            else if((persona.getSexo() == Sexo.HOMBRE && persona.getOrientacionSexual() == OrientacionSexual.BISEXUAL)
-                    && (candidatos.get(x).getOrientacionSexual() == OrientacionSexual.HETERO && candidatos.get(x).getSexo()== Sexo.MUJER)){
-                aux = candidatos.get(x);
-            }
-            else if(persona.getSexo() == Sexo.MUJER && persona.getOrientacionSexual() == OrientacionSexual.BISEXUAL
-                    && candidatos.get(x).getOrientacionSexual() == OrientacionSexual.LESBIANA){
-                aux = candidatos.get(x);
-            }
-            else if((persona.getSexo() == Sexo.MUJER && persona.getOrientacionSexual() == OrientacionSexual.BISEXUAL)
-                    && (candidatos.get(x).getOrientacionSexual() == OrientacionSexual.HETERO && candidatos.get(x).getSexo()== Sexo.HOMBRE))
-                aux = candidatos.get(x);
-            }
+        }
             
         
         return aux;
         
     }
+    
+    public boolean politica(Persona candidato, Persona persona){
+        boolean resultat = false;
+        
+        if(candidato.getOrientacionPolitica() == OrientacionPolitica.CENTRO && 
+         ((persona.getOrientacionPolitica() == OrientacionPolitica.DERECHA)||
+         (persona.getOrientacionPolitica() == OrientacionPolitica.CENTRO)||
+         (persona.getOrientacionPolitica() == OrientacionPolitica.IZQUIERDA))){
+            resultat = true;
+        }
+        else if(candidato.getOrientacionPolitica() == OrientacionPolitica.DERECHA &&
+                ((persona.getOrientacionPolitica() == OrientacionPolitica.EXTREMA_DERECHA)||
+                (persona.getOrientacionPolitica() == OrientacionPolitica.DERECHA)||
+                (persona.getOrientacionPolitica() == OrientacionPolitica.CENTRO))){
+            resultat = true;
+        }
+        else if(candidato.getOrientacionPolitica() == OrientacionPolitica.EXTREMA_DERECHA &&
+                ((persona.getOrientacionPolitica() == OrientacionPolitica.EXTREMA_DERECHA)||
+                (persona.getOrientacionPolitica() == OrientacionPolitica.DERECHA))){
+            resultat = true;
+        }
+        else if(candidato.getOrientacionPolitica() == OrientacionPolitica.IZQUIERDA &&
+                ((persona.getOrientacionPolitica() == OrientacionPolitica.EXTREMA_IZQUIERDA)||
+                (persona.getOrientacionPolitica() == OrientacionPolitica.IZQUIERDA)||
+                (persona.getOrientacionPolitica() == OrientacionPolitica.CENTRO))){
+            resultat = true;
+        }
+        else if(candidato.getOrientacionPolitica() == OrientacionPolitica.EXTREMA_IZQUIERDA &&
+                ((persona.getOrientacionPolitica() == OrientacionPolitica.EXTREMA_IZQUIERDA)||
+                (persona.getOrientacionPolitica() == OrientacionPolitica.IZQUIERDA))){
+            resultat = true;
+        }
+        
+        return resultat;
+    }
+    public boolean sexualitat(Persona candidato, Persona persona){
+        
+        boolean resultat = false;
+        
+        if((candidato.getOrientacionSexual() == OrientacionSexual.HETERO && candidato.getSexo() == Sexo.HOMBRE)&&
+          (persona.getOrientacionSexual() == OrientacionSexual.HETERO && persona.getSexo() == Sexo.MUJER)){
+            resultat = true;
+        }
+        else if((candidato.getOrientacionSexual() == OrientacionSexual.HETERO && candidato.getSexo() == Sexo.MUJER)&&
+          (persona.getOrientacionSexual() == OrientacionSexual.HETERO && persona.getSexo() == Sexo.HOMBRE)){
+            resultat = true;
+        }
+        else if((candidato.getOrientacionSexual() == OrientacionSexual.BISEXUAL && candidato.getSexo() == Sexo.MUJER)&&
+                ((persona.getOrientacionSexual() == OrientacionSexual.LESBIANA || persona.getOrientacionSexual() == OrientacionSexual.BISEXUAL)&&
+                persona.getSexo() == Sexo.MUJER)){
+            resultat = true;
+        }
+        else if((candidato.getOrientacionSexual() == OrientacionSexual.BISEXUAL && candidato.getSexo() == Sexo.HOMBRE)&&
+                ((persona.getOrientacionSexual() == OrientacionSexual.GAY || persona.getOrientacionSexual() == OrientacionSexual.BISEXUAL)&&
+                persona.getSexo() == Sexo.HOMBRE)){
+            resultat = true;
+        }
+        else if((candidato.getOrientacionSexual() == OrientacionSexual.BISEXUAL && candidato.getSexo() == Sexo.HOMBRE)&&
+                (persona.getOrientacionSexual() == OrientacionSexual.HETERO && persona.getSexo() == Sexo.MUJER)){
+            resultat = true;
+        }
+        else if((candidato.getOrientacionSexual() == OrientacionSexual.BISEXUAL && candidato.getSexo() == Sexo.MUJER)&&
+                (persona.getOrientacionSexual() == OrientacionSexual.HETERO && persona.getSexo() == Sexo.HOMBRE)){
+            resultat = true;
+        }
+        else if(candidato.getOrientacionSexual() == OrientacionSexual.GAY && persona.getOrientacionSexual() == OrientacionSexual.GAY){
+            resultat = true;
+        }
+        
+        else if(candidato.getOrientacionSexual() == OrientacionSexual.LESBIANA && persona.getOrientacionSexual() == OrientacionSexual.LESBIANA){
+            resultat = true;
+        }
+        
+        return resultat;
+    }
+    
 
     public MatcherImpl(Persona persona, List<Persona> candidatos) {
         this.persona = persona;
